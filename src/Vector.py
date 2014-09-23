7ce018678e610fe22cf85ded1c6c1bd961c07b6c
Ensis <pierre.ecormier@yahoo.fr>
2014-09-23 18:05:25 +0100

    add: Vector class and interactive shell

---
+++ src/Vector.py
@@ -0,0 +1,32 @@
+import Mat
+import Lexer
+from Parser import Parser
+from Interpreter import Interpreter
+
+class Vector:
+    def __init__(self, debug = False):
+        self.debug = debug
+        self.parser = Parser()
+        self.interpreter = Interpreter()
+
+    def eval(self, s):
+        if self.debug:
+            print "Input: " + s
+        lexemes = Lexer.lex(s)
+        if self.debug:
+            print "Lexemes: " + str(lexemes)
+        ast = self.parser.parse(lexemes)
+        if self.debug:
+            print "AST:"
+            self.printAST(ast, 0)
+        result = self.interpreter.eval(ast)
+        if self.debug:
+            print "Result: ",
+        print str(result)
+
+    def printAST(self, ast, indent):
+        print "| " * indent + str(ast[1])
+        if len(ast) < 3:
+            return
+        for c in ast[2]:
+            self.printAST(c, indent + 1)
